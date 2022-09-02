592f39d5638ddd7260d8215fce6a5e0b81cee290
Daniel Barros <daniel.barros@cqumail.com>
2022-09-02 13:37:46 +0800

    Stage 2

--- src/SimpleChattyBot.java
+++ src/SimpleChattyBot.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class SimpleChattyBot {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello! My name is Aid.");
         System.out.println("I was created in 2022.");
+        System.out.println("Please, remind me your name.");
+        System.out.print("> ");
+        String name = scanner.nextLine();
+        System.out.println("What a great name you have, " + name + "!");
     }
 }
