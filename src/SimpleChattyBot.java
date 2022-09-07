e8477beef0481ac779ef51da597d8aa1f1e426a0
Daniel Barros <daniel.barros@cqumail.com>
2022-09-07 14:56:59 +0800

    Stage 3

--- src/SimpleChattyBot.java
+++ src/SimpleChattyBot.java
@@ -9,5 +9,16 @@ public class SimpleChattyBot {
         System.out.print("> ");
         String name = scanner.nextLine();
         System.out.println("What a great name you have, " + name + "!");
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and" +
+                " 7.");
+        System.out.print("> ");
+        int remainder3 = scanner.nextInt();
+        System.out.print("> ");
+        int remainder5 = scanner.nextInt();
+        System.out.print("> ");
+        int remainder7 = scanner.nextInt();
+        int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+        System.out.println("Your age is " + age + "; that's a good time to start programming!");
     }
 }
