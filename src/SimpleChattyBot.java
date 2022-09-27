69026c38e18783220c8860d2fafa2689d3209af2
Daniel Barros <daniel.barros@cqumail.com>
2022-09-27 11:53:20 +0800

    Stage 4

--- src/SimpleChattyBot.java
+++ src/SimpleChattyBot.java
@@ -6,19 +6,23 @@ public class SimpleChattyBot {
         System.out.println("Hello! My name is Aid.");
         System.out.println("I was created in 2022.");
         System.out.println("Please, remind me your name.");
-        System.out.print("> ");
         String name = scanner.nextLine();
         System.out.println("What a great name you have, " + name + "!");
         System.out.println("Let me guess your age.");
         System.out.println("Enter remainders of dividing your age by 3, 5 and" +
                 " 7.");
-        System.out.print("> ");
         int remainder3 = scanner.nextInt();
-        System.out.print("> ");
         int remainder5 = scanner.nextInt();
-        System.out.print("> ");
         int remainder7 = scanner.nextInt();
         int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
-        System.out.println("Your age is " + age + "; that's a good time to start programming!");
+        System.out.println("Your age is " + age + "; that's a good time to " +
+                "start programming!");
+        System.out.println("Now I will prove to you that I can count to any " +
+                "number you want.");
+        int number = scanner.nextInt();
+        for (int i = 0; i <= number; i++) {
+            System.out.println(i + "!");
+        }
+        System.out.println("Completed, have a nice day!");
     }
 }
